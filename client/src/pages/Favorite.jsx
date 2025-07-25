c1739627f8d19658a927f32ac82398821f4e7fb9
puneeth-raj <puneeth.panayal@gmail.com>
2025-07-25 21:53:56 +0530

    ticket booking

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import BlurCircle from '../components/BlurCircle'
+import MovieCard from '../components/MovieCard'
 
 const Favorite = () => {
-  return (
-    <div>Favorite</div>
-  )
+  return dummyShowsData.length > 0? (
+  <div className='relative my-40 mb-60 px-6 md:px-16 1g:px-40 x1:px-44
+  overflow-hidden min-h-[80vh]'>
+    <BlurCircle top="150px" left="0px"/>
+    <BlurCircle bottom="50px" right="50px"/>
+    <h1 className='text-lg font-medium my-4' >Your Fav Movies</h1>
+    <div className='flex flex-wrap max-sm:justify-center gap-8'>
+      {dummyShowsData.map((movie) =>  (
+        <MovieCard movie={movie} key={movie._id}/>
+      ))}
+    </div>
+  </div>)
+  : (
+   <div className='flex flex-col items-center justify-center h-screen'>
+     <h1 className='text-3xl font-bold text-center' >No movies available</h1>
+   </div>)
 }
 
 export default Favorite
--- client/src/pages/MyBooking.jsx
+++ client/src/pages/MyBooking.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const MyBooking = () => {
-  return (
-    <div>MyBooking</div>
-  )
+
+    const currency = import.meta.env.CURRENCY
+    const [bookings, setBookings] = useState([])
+    const [isLoading,setIsLoading] = useState(true)
+
+    const getMyBookings = async ()=>{
+        setBookings(dummyBookingData)
+        setIsLoading(false)
+    }
+    useEffect(()=>{
+        getMyBookings()
+    })
+  return !isLoading?(
+
+    <div>
+        <p className=''>My Booling</p>
+    </div>
+
+  ):
+  <Loading/>
 }
 
 export default MyBooking
